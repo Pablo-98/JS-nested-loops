b7eb12c20f89c436a9caf6fa9056022c4b80d662
Pablo Hernandez <hpablo575@gmail.com>
2022-12-20 15:25:55 -0700

    started function

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// creating 
+// creating a working solution with nested loops (Time complexity: O(n^))
+
+
+function twoSum(nums,target) {
+    let result = [];
+
+    for(let i = 0; i < nums.length; i++) {
+        for(let j = i + 1; j<nums.length; j++ ){
+            if (nums[i] + nums[j] === target) {
+                
+            }
+        }
+    }
+}
