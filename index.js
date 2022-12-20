fc8dd5fa2af08c6f269c8c90ef4fc0e1730fbab4
Pablo Hernandez <hpablo575@gmail.com>
2022-12-20 15:28:24 -0700

    commented code

--- index.js
+++ index.js
@@ -2,12 +2,15 @@
 
 
 function twoSum(nums,target) {
+    // declaring a variable named 'result' and setting it equal to an empty array
     let result = [];
 
+    //the first for loop takes i, sets it to zero, and if i is less than 
+    //the length of the number a user inputs 
     for(let i = 0; i < nums.length; i++) {
         for(let j = i + 1; j<nums.length; j++ ){
             if (nums[i] + nums[j] === target) {
-                
+
             }
         }
     }
